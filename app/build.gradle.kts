6e6653dd76b71cde44898e04107cc4b581e4d902
MaheshXenia <mahesh.xenia@gmail.com>
2024-10-19 16:13:45 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@
         implementation("androidx.appcompat:appcompat:1.7.0")
         implementation("com.google.android.material:material:1.12.0")
         implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-        implementation("androidx.activity:activity:1.9.2")
+        implementation("androidx.activity:activity:1.9.3")
         implementation(files("libs/printer-lib-2.2.4.aar"))
         testImplementation("junit:junit:4.13.2")
         androidTestImplementation("androidx.test.ext:junit:1.2.1")
